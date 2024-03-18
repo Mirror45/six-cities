9a5556197d18d8d22b96db0a6f8ceba195fcf071
Mirror <yllikano@gmail.com>
2024-03-18 17:26:21 +0200

    module-1/task-1 Add pages Components

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,1 +1,13 @@
+import MainPages from '../pages/main-pages/main-pages';
 
+type AppPlacesFoundProps = {
+  countAmsterdam: number;
+}
+
+function App({countAmsterdam}: AppPlacesFoundProps): JSX.Element {
+  return (
+    <MainPages countAmsterdam={countAmsterdam}/>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/app/app';
 
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+const PlacesAmsterdam = {
+  countPlacesAmsterdam: 3
+};
+
+root.render(
+  <React.StrictMode>
+    <App countAmsterdam = {PlacesAmsterdam.countPlacesAmsterdam} />
+  </React.StrictMode>,
+);
